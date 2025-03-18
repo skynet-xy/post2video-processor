b615d4c69d13017e5ac2538a7eed655ee0fd178f
chung <me@chungn.com>
2025-03-18 18:24:27 +0700

    Remove test router from API routes

--- app/main.py
+++ app/main.py
@@ -1,13 +1,12 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
-from app.api.routes import video, test, reddit
+from app.api.routes import video, reddit
 
 app = FastAPI(title="Post 2 Video API")
 
 # Include routers
 app.include_router(video.router, prefix="/api")
-app.include_router(test.router, prefix="/api", tags=["test"])
 app.include_router(reddit.router, prefix="/api")
 
 # Mount the static directory
