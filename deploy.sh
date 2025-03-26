fb325f8d2f37bd266bd1c0456599b4b40d9bccd1
ducanh <ngducanh1223@gmail.com>
2025-03-26 22:03:45 +0100

    Fix Docker Compose file extension in deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 
 set -eux
 
-docker compose -f ./docker-compose.yaml build
-docker compose -f ./docker-compose.yaml up -d
+docker compose -f ./docker-compose.yml build
+docker compose -f ./docker-compose.yml up -d
